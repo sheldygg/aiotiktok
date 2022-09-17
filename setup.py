718d11629d46168df8ba0898b7f8db426167a1f2
sheldygg <chikibamtioma@gmail.com>
2022-09-17 09:36:26 +0300

    upload pypi

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def get_description():
     
 setuptools.setup(
     name="aiotiktok",
-    version="1.4",
+    version="1.5",
     license='MIT',
     author="sheldy",
     description="Tool for parse tiktok data",
